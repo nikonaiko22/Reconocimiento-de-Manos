edd94c7f98446a146049525931a8c1c614e468b4
Nicolás castro <nicolascastrocastro75@gmail.com>
2025-07-29 00:04:23 -0400

    Update Reconocimiento-manos.py

--- Reconocimiento-manos.py
+++ Reconocimiento-manos.py
@@ -9,7 +9,6 @@ cap = cv2.VideoCapture(url)
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
-
 with mp_hands.Hands(
         static_image_mode=False,
         max_num_hands=2,
@@ -21,13 +20,11 @@ with mp_hands.Hands(
         if not ret:
             print("No se pudo recibir imagen. Saliendo...")
             break
-
         
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         
         results = hands.process(rgb)
 
-        
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
